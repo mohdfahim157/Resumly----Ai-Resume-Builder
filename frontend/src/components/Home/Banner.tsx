cb9b79476fc9669d34ba46b50ff361fdd5bfe67e
Fahim-157 <fahimkhatri876@gmail.com>
2026-07-22 17:18:24 +0530

    connect frontend and backend 'connect login and signIn'

--- frontend/src/components/Home/Banner.tsx
+++ frontend/src/components/Home/Banner.tsx
@@ -1,9 +1,5 @@
-import {useBuilder} from "../../../context/Builder.context.ts"
-
-
 export default function Banner() {
-  const {data} = useBuilder()
-  console.log(data)
+
   return (
     
    <div className="w-full py-2.5 font-medium text-sm text-green-900 text-center bg-linear-to-r from-[#32c245] to-[#FDFEFF]">
